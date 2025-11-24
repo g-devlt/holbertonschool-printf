193d7da081c388fef8a4ae659c37c766c511c48f
B-ama-k <Benjamin.fln74@gmail.com>
2025-11-24 12:59:21 +0000

    modif of the name of function on main.h ((int write_char(char c) for (int write_char(va_list args) && (int write_string(char *str) for (int write_string(va_list args)) and modif write.c.

--- write.c
+++ write.c
@@ -1,13 +1,33 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
-* write_char - 
-* @c: 
-* Return:
+* write_char - Prints a single character.
+* @args: va_list containing the character to print.
+* Return: Number of characters printed (1)
 */
 
-int write_char(char c)
+int write_char(va_list args)
 {
-	return write(1, &c, 1);
+	char c = va_arg(args, int);
+	return (write(1, &c, 1));
+}
+
+/**
+* write_string - Prints a string.
+* @args: va_list containing the string to print.
+* Return: Number of characters printed
+*/
+
+int write_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int len = 0;
+
+	if (!s)
+		s = NULL;
+
+	while (s[len])
+		len++;
+
+	return (write(1, s, len));
 }
