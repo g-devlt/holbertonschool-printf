858a829f971cf3ce87bc90c8d658c35dcc571cd0
B-ama-k <Benjamin.fln74@gmail.com>
2025-11-24 10:57:57 +0000

    correct main.h and make write_char in writh.c

--- write.c
+++ write.c
@@ -1,10 +1,13 @@
 #include "main.h"
+#include <unistd.h>
 
- /**
- * write_char - 
- * @c: 
- * Return:
- */
+/**
+* write_char - 
+* @c: 
+* Return:
+*/
 
 int write_char(char c)
-
+{
+	return write(1, &c, 1);
+}
